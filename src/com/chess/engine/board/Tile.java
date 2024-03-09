e031811230eb0889d05cbdfb4288951343d4e661
Seishin LeBlanc <seishinleblanc@gmail.com>
2024-03-09 12:34:33 -0600

    Created Board + Move classes and Alliance enum.

--- src/com/chess/engine/board/Tile.java
+++ src/com/chess/engine/board/Tile.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public abstract class Tile {
     protected final int tileCoordinate;
 
-    private static final Map<Integer, EmptyTile > EMPTY_TILES = createAllPossibleEmptyTiles();
+    private static final Map<Integer, EmptyTile > EMPTY_TILES_CACHE = createAllPossibleEmptyTiles();
 
     private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
 
@@ -23,7 +23,7 @@ public abstract class Tile {
     }
 
     public static Tile createTile(final int tileCoordinate, final Piece piece) {
-        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES.get(tileCoordinate);
+        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES_CACHE.get(tileCoordinate);
     }
 
     private Tile(int tileCoordinate) {
@@ -35,7 +35,7 @@ public abstract class Tile {
     public abstract Piece getPiece();
 
     public static final class EmptyTile extends Tile {
-        EmptyTile(final int coordinate) {
+        private EmptyTile(final int coordinate) {
             super(coordinate);
         }
         @Override
@@ -51,7 +51,7 @@ public abstract class Tile {
 
     public static final class OccupiedTile extends Tile {
         private final Piece pieceOnTile;
-        OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
+        private OccupiedTile(int tileCoordinate, Piece pieceOnTile) {
             super(tileCoordinate);
             this.pieceOnTile = pieceOnTile;
         }
